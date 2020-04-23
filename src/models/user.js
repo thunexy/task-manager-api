111d9fd38cf7098ed53c1bea198d8e5e65addffe
Adetoye Oyebisi <adetoye.oyebisi@interswitchgroup.com>
2020-04-23 16:47:52 +0100

    Corrected jwt secret

--- src/models/user.js
+++ src/models/user.js
@@ -63,7 +63,7 @@ userSchema.virtual('tasks', {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()}, process.env.JSW_SECRET)
+    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({token})
     await user.save()
     return token;
